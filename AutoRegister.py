7f161f38129645718d9fda117e5f6a30825a1192
tinalatif <tina.tl.latif@gmail.com>
2013-11-30 02:50:41 -0500

    Single-course single-try NOT through quickadd because of issues with users getting banned lol

--- AutoRegister.py
+++ AutoRegister.py
@@ -5,51 +5,101 @@
 
 import getpass
 import mechanize
+import cgi
 import cookielib
 import os
 import time
+from bs4 import BeautifulSoup
 
 loginPage = 'https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin'
 logoutPage = 'https://horizon.mcgill.ca/pban1/twbkwbis.P_Logout'
-quickAddPage = 'https://horizon.mcgill.ca/pban1/bwskfreg.P_AltPin'
+addPage = 'https://horizon.mcgill.ca/pban1/bwskfcls.p_sel_crse_search'
 
-username = raw_input("Username: (firstname.lastname) \n")
-username += "@mail.mcgill.ca"
-password = getpass.getpass(prompt="Password: (hidden)\n")
-CRN = raw_input("CRN you want to enroll in: \n")
-semester = raw_input("Semester you want to enroll in (FALL or WINTER): \n")
+# username = raw_input("Username: (firstname.lastname) \n")
+# username += "@mail.mcgill.ca"
+# password = getpass.getpass(prompt="Password: (hidden)\n")
+
 
 br = mechanize.Browser()
 cj = cookielib.CookieJar()
 br.set_cookiejar(cj)
 
-for i in range (2016):
+def login():
 	br.open(loginPage)
 	br.select_form(nr=1)
-	br.form['sid'] = username
-	br.form['PIN'] = password
+	br.form['sid'] = raw_input("Username: (firstname.lastname) \n") + "@mail.mcgill.ca"
+	br.form['PIN'] = getpass.getpass(prompt="Password: (hidden)\n")
 	br.submit()
+	
 
-	br.open(quickAddPage)
+def selectSemester(semester):
+	br.open(addPage)
 	br.select_form(nr=1)
-	if semester == 'FALL':
-		br.form['term_in'] = ['201309']
-	elif semester == 'WINTER':
-		br.form['term_in'] = ['201401']
-	br.submit()
+	if semester == "WINTER":
+		br.form["p_term"] = ["201401"]
+		response = br.submit()
+	elif semester == "FALL":
+		print "Fall not implemented yet lol"
+	else:
+		print "I said FALL or WINTER"
 
+def searchForCourse(courseSubject, courseNum):
 	br.select_form(nr=1)
-	br.form.set_value(CRN, name='CRN_IN', type='text', nr=0)
+	
+	subjControl = br.find_control(name='sel_subj', type="select")
+	subjControl.value = [courseSubject]
+	
+	br.form['sel_crse'] = courseNum
+	
+	# set faculty to null because it takes default val of 'MG'
+	br.form['sel_coll'] = []
+	
 	br.submit()
-
 	resultPage = br.response().read()
-	resultPageFile = open('page.html', 'w+')
-	resultPageFile.write(resultPage)
-	resultPageFile.close()
+	if 'No classes were found' in resultPage:
+		return null
 
-	if 'Registration Add Errors' not in resultPage:
-		os.system('growlnotify -s -t "Registration??" -m "Maybe!"')					 
+	soup = BeautifulSoup(resultPage)
+	table = soup.find('table', summary='This layout table is used to present the sections found')
+	return table
 
-	br.open(logoutPage)
+def canRegister(courseTable):
+	tds = table.findAll('tr')[2]('td')
+	rem = int(tds[12].string)
+	status = tds[19].string 
+	if rem > 0 and status == 'Active':
+		return True
+	return False
 
-	time.sleep(600)
+def canJoinWaitlist(courseTable):
+	tds = table.findAll('tr')[2]('td')
+	rem = int(tds[15].string)
+	status = tds[19].string 
+	if rem > 0 and status == 'Active':
+		return True
+	return False
+
+# Main
+login()
+
+course = raw_input("Course you want to enroll in (e.g. COMP 330): \n")
+semester = raw_input("Semester you want to enroll in (FALL or WINTER): \n")
+courseSubject = course.split()[0]
+courseNum = course.split()[1]
+
+selectSemester(semester)
+
+table = searchForCourse(courseSubject, courseNum)
+if table is None:
+	print "That class doesn't appear to be offered in that semester!"
+else:
+	if canRegister(table):
+		print "Spot available for registration"
+		# Add to worksheet and stuff
+	else:
+		print "No spots available for registration"
+		# check for waitlist
+		if canJoinWaitlist(table):
+			print "Spot available on the waitlist"
+		else:
+			print "No spots available on the waitlist"
